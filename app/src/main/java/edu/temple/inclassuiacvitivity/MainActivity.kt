f6f2d7b5b4c838c10e6624880ff24b6d1dfde78b
t00nny <129798251+t00nny@users.noreply.github.com>
2024-09-19 14:49:10 -0400

    first version

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -1,12 +1,10 @@
 package edu.temple.inclassuiacvitivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.AdapterView.OnItemSelectedListener
-
-// TODO Replace ArrayAdapter with your implementation of TextSizeAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,28 +14,26 @@ class MainActivity : AppCompatActivity() {
         val spinner = findViewById<Spinner>(R.id.spinner)
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
-        /* Step 1: Populate this array */
-        val numberArray = Array(50){i -> (i + 1) * 2}
+        // Step 1: Populate this array
+        val numberArray = Array(50) { i -> (i + 1) * 2 }
 
-        with (spinner) {
-            /* Step 2: Create adapter to display items from array in Spinner */
-            adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_spinner_item, numberArray)
+        with(spinner) {
+            // Step 2: Create adapter to display items from array in Spinner using TextSizeAdapter
+            adapter = TextSizeAdapter(numberArray)
 
-
-            // Step 3: Change TextView's text size to the number selected in the Spinner */
+            // Step 3: Change TextView's text size to the number selected in the Spinner
             onItemSelectedListener = object : OnItemSelectedListener {
                 override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                     parent?.run {
+                        // Set the TextView's text size to the selected number
                         displayTextView.textSize = getItemAtPosition(position).toString().toFloat()
                     }
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {
-
+                    // Do nothing if no selection
                 }
-
             }
         }
-
     }
 }
--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,39 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val sizes: Array<Int>) : BaseAdapter() {
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        // Return the number of items in the array
+        return sizes.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        // Return the size at the given position
+        return sizes[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        // Return the item's ID (in this case, we just return the position as the ID)
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
-    }
+        val context = parent?.context ?: throw IllegalStateException("Parent view group is null")
+        val textView = convertView as? TextView ?: TextView(context)
 
+        // Set the text and size for each dropdown item based on the array
+        val size = sizes[position]
+        textView.text = size.toString()
+
+        // Set the text size to represent the size value when in dropdown
+        textView.textSize = size.toFloat()
+
+        return textView
+    }
 }
